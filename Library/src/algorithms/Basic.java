7e55305180cca3adc384e10c05479169f2e21421
Tijana <vujicic.tijana@gmail.com>
2017-04-29 15:03:38 +0200

    Test

--- Library/src/algorithms/Basic.java
+++ Library/src/algorithms/Basic.java
@@ -2,18 +2,20 @@ package algorithms;
 
 import calculations.BasicCalcs;
 import calculations.Calculations;
+
 public class Basic {
+	//change calcs
 	private double alpha;
 	private double beta;
 	private double[] expectedY;
 	private Calculations calcs;
 
-	public Basic(double alpha, double beta, double[][] s, double[] r, double[] expectedY,Calculations calcs) {
+	public Basic(double alpha, double beta, double[][] s, double[] r, double[] expectedY, Calculations calcs) {
 		super();
 		this.alpha = alpha;
 		this.beta = beta;
 		this.expectedY = expectedY;
-		this.calcs =calcs;
+		this.calcs = calcs;
 	}
 
 	public double[] predictOutputs() {
